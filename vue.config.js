4beedfd2c66a16bfcd390664c797656cc9b9c3b5
emarinfer <enriquemarin_sierra@hotmail.com>
2021-06-22 00:35:59 +0200

    Add vue.config.js file

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    pages: {
+      index: {
+        entry: 'src/main.js',
+        title: 'My Maps with Leaflet'
+      },
+      404: {
+        entry: 'src/main.js',
+        template: "public/index.html",
+        filename: "404.html",
+        title: 'My Maps with Leaflet'
+      },
+    },
+}
